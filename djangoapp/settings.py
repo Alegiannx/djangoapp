3fc078d55dd7b52305c47ec669fa4eb1e0594785
Alegiannx <alegiannx@gmail.com>
2020-11-22 14:31:24 +0200

    add react frontend

--- djangoapp/settings.py
+++ djangoapp/settings.py
@@ -11,16 +11,16 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o8_4=)wg+=h_2t=5y79gjwxf%5q1fjds*@&x!306&6$-#rqxpx'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
